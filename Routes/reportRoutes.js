ff1d89e6d203436e860ecdcdefeedf7204193351
Abdullahi Omar <xudayfoamer@gmail.com>
2024-11-06 09:05:34 +0300

    Adding last update

--- Routes/reportRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const reportController = require('../controllers/reportController');
-
-// Report Routes
-router.post('/reports', reportController.createReport);
-router.get('/reports', reportController.getReports);
-
-module.exports = router;
